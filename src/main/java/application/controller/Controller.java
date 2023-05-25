300bc99ea84f4c03072f0faab73e7e09518d1cac
dinmukhammed_ospanov <dinmukhvmmed176@gmail.com>
2023-05-25 18:48:56 +0600

    Update Controller, add delete and post methods. Fix bugs.

--- src/main/java/application/controller/Controller.java
+++ src/main/java/application/controller/Controller.java
@@ -5,9 +5,7 @@ import application.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +15,28 @@ public class Controller {
     private StudentMapper studentMapper;
 
     @GetMapping("/")
-    private ResponseEntity getAllStudents() {
+    public ResponseEntity getAllStudents() {
         List<Student> students = studentMapper.findAllStudents();
         return new ResponseEntity(students, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity addStudent(@RequestParam(name = "name") String name,
+                                      @RequestParam(name = "surname") String surname,
+                                      @RequestParam(name = "age") int age,
+                                      @RequestParam(name = "average grade") double averageGrade) {
+        Student student = new Student();
+        student.setName(name);
+        student.setSurname(surname);
+        student.setAge(age);
+        student.setAverageGrade(averageGrade);
+        studentMapper.addStudent(student);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity deleteStudent(@RequestParam(name = "id") int id) {
+        studentMapper.deleteStudent(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }

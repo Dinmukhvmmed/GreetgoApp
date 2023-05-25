bfb8ec4d18e1f64d9cb417f64c2911557c1cc5ca
dinmukhammed_ospanov <dinmukhvmmed176@gmail.com>
2023-05-25 17:44:38 +0600

    Implemented config files for Spring and MyBatis. Create mapper xml file for Student model. Add gitignore file.

--- src/main/java/application/MyApplication.java
+++ src/main/java/application/MyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("application.mapper")
+@MapperScan("src.main.resources.mappers")
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);

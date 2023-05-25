4963f97e830a33ec278e18b0529ff5c42f7ec3e4
dinmukhammed_ospanov <dinmukhvmmed176@gmail.com>
2023-05-25 18:16:22 +0600

    Fix bugs in MyBatis config after create Controller

--- src/main/java/application/MyApplication.java
+++ src/main/java/application/MyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("src.main.resources.mappers")
+@MapperScan("application.mapper")
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
--- src/main/java/application/controller/Controller.java
+++ src/main/java/application/controller/Controller.java
@@ -1,7 +1,24 @@
 package application.controller;
 
+import application.mapper.StudentMapper;
+import application.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @GetMapping("/")
+    private ResponseEntity getAllStudents() {
+        List<Student> students = studentMapper.findAllStudents();
+        return new ResponseEntity(students, HttpStatus.OK);
+    }
 }
